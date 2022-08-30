be3fbe67e6879da54f6d5f4ff9e6cc08b4aa6a3d
Alex Casadevall <alex@lexartlabs.com>
2022-08-30 17:51:43 -0300

    update app

--- frontend/app-03/app.js
+++ frontend/app-03/app.js
@@ -5,21 +5,39 @@ const app = new Vue({
             nombre: "",
             completado: false
         },
-        lista: []
+        lista: [],
+        indiceDinamico: undefined
     },
     methods: {
+        guardarMemoria: function (){
+            const LISTA_MEMORIA = JSON.stringify(this.lista)
+            window.localStorage.setItem('_lista_app', LISTA_MEMORIA)
+        },
         agregarItem: function (){
             const item = {...this.item} // Construir un objeto de 0
-            this.lista.push(item) // Metodo .push(objeto) agrega el obheto para dentro de la lista
+             
+
+            if(this.indiceDinamico != undefined){
+                // this.lista[this.indiceDinamico] = item // Est. de JS
+                this.$set(this.lista, this.indiceDinamico, item) // Est. de datos de Vue
+            } else {
+                this.lista.push(item) // Metodo .push(objeto) agrega el obheto para dentro de la lista
+            }
+
+            // Agrego o edito un item
+            // Guardo en memoria
+            this.guardarMemoria()
 
             // Una vez que agrego a la lista
             // Reinicio los valores del objeto item
             this.item.nombre = ""
             this.item.completado = false
+            this.indiceDinamico = undefined
         },
         completarItem: function (indice){
             this.lista[indice].completado = true
             console.log("this.lista: ", this.lista)
+            this.guardarMemoria()
         },
         eliminarItem: function (indice){
             // El splice remueve un elemento de la lista
@@ -27,6 +45,29 @@ const app = new Vue({
             if(this.lista[indice].completado == false){ // Si no esta completado el item de la lista
                 this.lista.splice(indice, 1)
             }
+            this.guardarMemoria()
+        },
+        editarItem: function (indice){
+            // Agarro manzana que esta en la posicion 0
+            if(this.lista[indice].completado == false){
+                this.item.nombre     = this.lista[indice].nombre
+                this.item.completado = this.lista[indice].completado
+
+                this.indiceDinamico  = indice
+            }
+            // this.item.nombre     -> Manzana
+            // this.item.completado -> false
+        }
+    },
+    mounted(){
+        console.log("Pagina cargada :: ")
+        const LISTA_MEMORIA = window.localStorage.getItem('_lista_app')
+        console.log(LISTA_MEMORIA)
+        
+        if(LISTA_MEMORIA == null){
+            this.lista = []
+        } else {
+            this.lista = JSON.parse(LISTA_MEMORIA)
         }
     }
 })
